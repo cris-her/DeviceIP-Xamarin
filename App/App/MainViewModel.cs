3b161c9e35a040162721485f4f1254db8c18acc9
chernandez <chernandez@hasar.com.mx>
2022-01-06 13:30:53 -0600

    added get external ip

--- App/App/MainViewModel.cs
+++ App/App/MainViewModel.cs
@@ -1,27 +1,43 @@
 ﻿using App.Services;
 using System;
 using System.Collections.Generic;
+using System.Net;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace App
 {
     internal class MainViewModel : ViewModelBase
     {
         private readonly IIPAddressManager _ipAddressManager;
-        private String _ipAddress;
+        private string _ipAddress;
         public MainViewModel(IIPAddressManager ipAddressManager)
         {
             _ipAddressManager = ipAddressManager;
         }
-        public String IpAddress
+        public string IpAddress
         {
             get => _ipAddress;
             set => Set(ref _ipAddress, value);
         }
         public void OnAppeared()
         {
-            IpAddress = _ipAddressManager.GetIPAddress();
+            //IpAddress = _ipAddressManager.GetIPAddress();
             //IpAddress = "Hello world";
+            getExternalIp();
+        }
+
+        public string getExternalIp()
+        {
+            try
+            {
+                //string externalIP;
+                IpAddress = (new WebClient()).DownloadString("http://checkip.dyndns.org/");
+                IpAddress = (new Regex(@"\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}"))
+                             .Matches(IpAddress)[0].ToString();
+                return IpAddress;
+            }
+            catch { return null; }
         }
 
     }
